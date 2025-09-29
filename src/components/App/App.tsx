66f7641add60d3c9f98dba0e75ddabb8d5c79e0b
VsemDobra <ivanpampuha88@gmail.com>
2025-09-29 12:50:29 +0300

    update names

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import { MovieGrid } from "../MovieGrid/MovieGrid";
 import { Loader } from "../Loader/Loader";
 import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
 import { MovieModal } from "..//MovieModal/MovieModal";
-import { fetchMovies } from "../../services/o";
+import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 import styles from "./App.module.css";
 
